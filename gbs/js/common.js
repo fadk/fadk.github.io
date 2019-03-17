808e8a6f47997034f2c76b3fc874248512cadaac
fadk <fadejeva.k34@gmail.com>
2019-03-17 23:27:43 +0300

    init

---
+++ gbs/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        nav: true,
+        navText: [],
+        lazyLoad: true,
+        video: true,
+    });
+});
