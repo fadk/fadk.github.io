5158bcd2bfffa93881e027d02ac4bb90e447bf26
fadk <fadejeva.k34@gmail.com>
2019-04-26 11:37:50 +0300

    inner

--- ciciban/js/common.js
+++ ciciban/js/common.js
@@ -133,3 +133,14 @@ function topFunction() {
 })()
 
 
+$(document).ready(function() {
+    $('#tabs li').on('click', function() {
+        var tab = $(this).data('tab');
+
+        $('#tabs li').removeClass('is-active');
+        $(this).addClass('is-active');
+
+        $('#tab-content div').removeClass('is-active');
+        $('div[data-content="' + tab + '"]').addClass('is-active');
+    });
+});
